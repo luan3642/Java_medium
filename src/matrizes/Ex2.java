231ac2ca683910700b406ed8e04219e3af402ef7
luan3642 <luan3642@hotmail.com>
2025-01-10 11:20:01 -0300

    matriz return

--- src/matrizes/Ex2.java
+++ src/matrizes/Ex2.java
@@ -20,7 +20,13 @@ public class Ex2 {
 
         for(int i = 0; i<matriz.length; i++){
             for(int j =0; j<matriz[i].length; j++){
-                System.out.print(1);
+                if(i == j){
+                    matriz[i][j] = 1;
+                }else{
+                    matriz[i][j] = 0;
+                }
+
+
             }
             System.out.println();
         }
