1bc31d389d5504e2f1ad3533b5ca66048950b3a2
luan3642 <luan3642@hotmail.com>
2025-01-17 12:35:27 -0300

    matriz return

--- src/matrizes/Ex8.java
+++ src/matrizes/Ex8.java
@@ -3,30 +3,44 @@ package matrizes;
 public class Ex8 {
     public static void main(String[] args) {
         int[][] matriz = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
         };
 
-        int linhas = matriz.length;
-        int colunas = matriz[0].length;
+        int topo = 0;
+        int baixo = matriz.length - 1;
+        int esquerda = 0;
+        int direita = matriz[0].length - 1; // Correção para número de colunas
 
-        System.out.println("linhas: " +linhas);
-        System.out.println("colunas " +colunas);
-
-        for(int i = 0; i<matriz.length; i++){
-            for(int j = 0; j<matriz[0].length; j++){
-                if(i == 0){
-                    System.out.print(matriz[i][j] + " ");
-                } else{
-                    System.out.println(matriz[i][j] );
-                }
-
-
-
+        while (topo <= baixo && esquerda <= direita) {
+            // Percorrer da esquerda para a direita no topo
+            for (int i = esquerda; i <= direita; i++) {
+                System.out.print(matriz[topo][i] + " ");
             }
-            System.out.println();
+            topo++;
 
+            // Percorrer de cima para baixo na direita
+            for (int i = topo; i <= baixo; i++) {
+                System.out.print(matriz[i][direita] + " ");
+            }
+            direita--;
+
+            // Percorrer da direita para a esquerda na base
+            if (topo <= baixo) {
+                for (int i = direita; i >= esquerda; i--) {
+                    System.out.print(matriz[baixo][i] + " ");
+                }
+                baixo--;
+            }
+
+            // Percorrer de baixo para cima na esquerda
+            if (esquerda <= direita) {
+                for (int i = baixo; i >= topo; i--) {
+                    System.out.print(matriz[i][esquerda] + " ");
+                }
+                esquerda++;
+            }
         }
     }
 }
